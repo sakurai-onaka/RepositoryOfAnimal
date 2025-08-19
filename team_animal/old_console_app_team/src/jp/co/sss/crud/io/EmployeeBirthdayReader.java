ef603b0e493a21427cfeafec53cba2298a8f9e85
sakurai hojo <h-sakurai@k-shared.co.jp>
2025-08-19 15:42:47 +0900

    日付入力チェッククラス作成終了

--- team_animal/old_console_app_team/src/jp/co/sss/crud/io/EmployeeBirthdayReader.java
+++ team_animal/old_console_app_team/src/jp/co/sss/crud/io/EmployeeBirthdayReader.java
@@ -1,6 +1,10 @@
 package jp.co.sss.crud.io;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  * 社員の生年月日をコンソール入力をするクラス
@@ -12,17 +16,29 @@ public class EmployeeBirthdayReader {
 	 * @throws IOException 不正な状態の場合にスローされる
 	 * @throws IllegalArgumentException 不正な入力の場合にスローされる
 	 */
-	public String input() throws IOException, IllegalArgumentException {
-		return null;
+	public String input() throws IOException, IllegalArgumentException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inputString = br.readLine();
+		if(isValid(inputString)) {
+			return inputString;
+		}else {
+			throw new IllegalArgumentException("正しい形式(西暦年/月/日)で日付を入力してください");
+		}
 	}
 
 	/**
 	 * @param inputString コンソール入力された文字列
 	 * @return 文字列が適正な値であった場合true、そうでない場合はfalseを返す
 	 */
-	public boolean isValid(String inputString) {
-
-		return false;//適正な日付であるかどうかをチェック
+	public boolean isValid(String inputString){
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		sdf.setLenient(false);
+		try {
+			sdf.parse(inputString);
+			return true;
+		}catch(ParseException e) {	
+			return false;//適正な日付であるかどうかをチェック
+		}
 	}
 
 }
