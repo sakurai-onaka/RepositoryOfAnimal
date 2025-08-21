b48d80cd92cc4211f8cdb8c1eb7a123eb32e6400
sakurai hojo <h-sakurai@k-shared.co.jp>
2025-08-21 11:19:21 +0900

    設計書　追加機能　登録処理　追記 登録系SQL文の修正

--- team_animal/old_console_app_team/src/jp/co/sss/crud/util/ConstantSQL.java
+++ team_animal/old_console_app_team/src/jp/co/sss/crud/util/ConstantSQL.java
@@ -21,7 +21,7 @@ public class ConstantSQL {
 	public static String SQL_FIND_BY_DEPTID = "SELECT emp_id, emp_name, gender, TO_CHAR(birthday, 'yyyy/MM/dd') AS birthday, dept_name FROM employee e INNER JOIN department d ON e.dept_id = d.dept_id WHERE e.dept_id = ? AND delete_flag = 0 ORDER BY emp_id";
 
 	/** 登録 */
-	public static final String SQL_INSERT = "INSERT INTO employee VALUES (seq_emp.nextval,?,?,?,?)";
+	public static final String SQL_INSERT = "INSERT INTO employee VALUES (seq_emp.nextval,?,?,?,?,?,?,0)";
 
 	/** 更新 */
 	public static final String SQL_UPDATE = "UPDATE employee SET emp_name = ?, gender = ?, birthday = ?, dept_id = ? WHERE emp_id = ?";
